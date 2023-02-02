be06635077ef87654309968dd49ffe9f4f0f57d7
YenChingY <123427804+YenChingY@users.noreply.github.com>
2023-02-02 11:05:25 +0000

    Intro to uni tests, and data types

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,15 @@
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 5;//int wholev number type
+    auto dec = 5.5;//double
+
+    cout<<"Num is integer: "<<num<<"\n";
+    cout<<"Dec is double: "<<dec<<"\n";
+    
 
     return 0;
 }
