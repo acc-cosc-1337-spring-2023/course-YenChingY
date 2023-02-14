c45ec4856370559fc3c22b25234ec11f7a00dd10
YenChingY <123427804+YenChingY@users.noreply.github.com>
2023-02-14 01:23:43 +0000

    Repetition, while, for, and do loop

--- src/examples/04_repetition/01_while/main.cpp
+++ src/examples/04_repetition/01_while/main.cpp
@@ -1,7 +1,32 @@
 #include "while.h"
+#include<iostream>
+
+using std::cin; using std::cout;
+using std::string;
 
 int main() 
 {
+	auto num = 0;
+	auto choice = 'n';
+
+	do
+	{
+		cout<<"Enter a number :";
+	cin>>num;
+
+	auto result = sum_of_squares(num);
+	cout<<"Sum of squares: "<<result<<"\n";
+
+	cout<<"Continue, enter y...: ";
+	cin>>choice;
+		
+	} 
+	while (choice == 'y' || choice == 'Y');
+	
+	
+	/*string name = "Hello";
+	display(name);*/
+
 
 	return 0;
 }
--- src/examples/04_repetition/01_while/while.cpp
+++ src/examples/04_repetition/01_while/while.cpp
@@ -1,9 +1,25 @@
+#include<iostream>
 #include "while.h"
+
+using std::cout;
+using std::string;
+
 //Using a while loop write code for sum_of_squares function that accepts an int and returns  
 //the sum of all the squares of that number from 1 to the number. 
 //Example: sum_of_squares(4) 
 //1*1 + 2 * 2 + 3*3 + 4*4 = 30
+int sum_of_squares(int num)
+{
+    auto sum = 0;
 
+    while(num > 0)
+    {
+        sum = num * num + sum;
+        num--;//decrement 1 from the current value of num
+    }
+
+    return sum;
+}
 
 //Using a while loop write code for display function that accepts a string and displays 
 //each character of the string in a line
@@ -18,4 +34,14 @@ l
 0
 
 */
+void display(std::string str)
+{
+    auto index = 0;
+    
+    while(index < str.length())
+    {
+        cout<<str[index]<<"\n";
+        index++;//increment 1 for the current value of index
 
+    }
+}
