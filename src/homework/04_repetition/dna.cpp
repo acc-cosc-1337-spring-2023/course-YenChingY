d72c219c37362ab17b5011fa1e341e9341a0f9c8
YenChingY <123427804+YenChingY@users.noreply.github.com>
2023-03-05 05:24:22 +0000

    Homework4: do wihle loop

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,40 @@
 //add include statements
+#include "dna.h"
+#include<iostream>
 
+using std::cout;
 //add function(s) code here
+
+int factorial(int num)
+{
+    auto sum = 1;
+
+    while(num > 0)
+    {
+        sum = sum * num; 
+        num--;
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+	while(num1 != num2)
+	{
+		auto temp = 0;
+		if(num1 < num2)
+        {
+            temp = num1; //do option 1
+            num1 = num2;
+            num2 = temp;
+		}	
+		else if(num1 > num2)
+		{
+			num1 = num1 - num2;	//do option 2
+		}
+
+	}
+	//cout<<"complete"<<"\n";
+	return num1; 
+
+}
