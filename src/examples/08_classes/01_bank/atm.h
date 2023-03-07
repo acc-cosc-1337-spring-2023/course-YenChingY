a276fb50684a2e305bc2977335c98678f508d1f3
YenChingY <123427804+YenChingY@users.noreply.github.com>
2023-03-07 02:21:22 +0000

    class inheritance, class func overrides, atm

--- src/examples/08_classes/01_bank/atm.h
+++ src/examples/08_classes/01_bank/atm.h
@@ -1,1 +1,7 @@
 //atm.h
+#include "bank_account.h"
+#include<iostream>
+
+void display_menu();
+void run_menu(BankAccount &account);
+void handle_menu_option(int option, BankAccount& account);
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,46 @@
+#include<iostream>
+#include<time.h>
+#include "checking_account.h"
+#include "atm.h"
+#include "savings_account.h"
+
+using std::cout; using std::cin;
 
 int main()
 {
+	srand(time(NULL));//generate true randoms on each main run execution
+	SavingsAccount savings;
+
+	CheckingAccount account;//customer 1
+	
+	auto choice = 's';
+
+	cout<<"Use checking or savings?";
+	cin>>choice;
+
+	if(choice == 'c')
+	{
+		cout<<account;
+		cout<<account.get_balance()<<"\n";
+		show_balance(account);//use the friend free function
+		run_menu(account);
+	}
+	else
+	{
+		cout<<savings;
+		cout<<savings.get_balance()<<"\n";
+		show_balance(savings);//use the friend free functio
+		run_menu(savings);
+	}
+
+
+	
+	cout<<account.get_balance()<<"\n";
+	
+	
+	//cout<<"Balance: "<<account.get_balance()<<"\n";
+	//CheckingAccount account1(100);//customer 2 for initial balance
+	//cout<<"Balance: "<<account1.get_balance()<<"\n";
 
 	return 0;
 }
