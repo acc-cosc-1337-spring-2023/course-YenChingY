65919296272d0b831305c1a3c7e0b7f57b60cb44
YenChingY <123427804+YenChingY@users.noreply.github.com>
2023-04-13 10:43:14 +0000

    two dimension stack array, multiplication table

--- src/examples/10_arrays/02_arrays_access/main.cpp
+++ src/examples/10_arrays/02_arrays_access/main.cpp
@@ -1,3 +1,8 @@
+#include "arrays_access.h"
+
 int main()
 {
+    int times_table[ROWS][COLS];//create empty list
+    populate_times_table(times_table, ROWS);
+    display_times_table(times_table, ROWS);
 }
