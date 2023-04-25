f5c47bbb6450f90f7b29e0a6d9d16066c0448be4
YenChingY <123427804+YenChingY@users.noreply.github.com>
2023-04-25 16:43:15 +0000

    lvalue and rvalue

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -4,8 +4,12 @@
 int main()
 {
     //std::cout<<"load use_vector to the stack\n";
-    use_vector();
+    //use_vector();
     //std::cout<<"unload use_vector from the stack\n";
+    int num = 5;
+    int& num_ref = num;
+    //5 = num; we can't do this; becasue 5 is not a variable
+    //int& five = 5;
 
     return 0;
 }
